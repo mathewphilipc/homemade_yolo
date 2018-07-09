13a2d406bb302d48d37d733c5efb4161813c5d64
Mathew Calkins <mathewpcalkins@gmail.com>
2018-07-09 18:53:24 -0400

    added blank solution

---
+++ solution.py
@@ -0,0 +1,17 @@
+def contains_banana(img):
+	"""
+	Change the contents of this function so it behaves correctly
+	"""
+	return -3.14159
+
+def crop_image(img, quadrant):
+	"""
+	Change the contents of this function so it behaves correctly
+	"""
+	return img
+
+def find_banana(img):
+	"""
+	Change the contents of this function so it behaves correctly
+	"""
+	return "None"
